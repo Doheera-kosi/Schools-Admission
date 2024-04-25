5a7630c5dd2aaeb40281cefe08872a9440f694c0
Doheera-kosi <kupourevans99@gmail.com>
2024-04-25 20:14:09 +0000

    Modified schools

--- Schools_Admission/schools/urls.py
+++ Schools_Admission/schools/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include #type: ignore
 from rest_framework.routers import DefaultRouter #type: ignore
 from .views import SchoolViewSet
+from .views import SchoolStudentsDetailView
+
 
 router = DefaultRouter()
 router.register(r'schools', SchoolViewSet, basename='schools')
@@ -8,4 +10,6 @@ router.register(r'schools', SchoolViewSet, basename='schools')
 urlpatterns = [
     path('', include(router.urls)),
     path('api/', include(router.urls)),
+    path('schools/<int:pk>/students/', SchoolStudentsDetailView.as_view(), name='school-students-detail'),
+
 ]
--- Schools_Admission/schools/views.py
+++ Schools_Admission/schools/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render #type: ignore
 
 # Create your views here.
-from rest_framework import viewsets #type: ignore
+from rest_framework import viewsets, generics, status #type: ignore
 from .models import School
 from .serializers import SchoolSerializer #type: ignore
+from students.models import Student
+from students.serializers import StudentSerializer
+from rest_framework.response import Response        #type: ignore
+
 
 class SchoolViewSet(viewsets.ModelViewSet):
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
     
 
+class SchoolStudentsDetailView(generics.RetrieveAPIView):
+    queryset = School.objects.all()
+    serializer_class = SchoolSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+
+        # Retrieve all admitted students for the current school
+        admitted_students = Student.objects.filter(school=instance.id)
+        student_serializer = StudentSerializer(admitted_students, many=True)
+
+        # Construct the response data
+        response_data = {
+            'school_details': serializer.data,
+            'admitted_students': student_serializer.data
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
