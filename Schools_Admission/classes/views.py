1d2b9956112f5edb171c6ea84a67bbbe6d2891c8
Doheera-kosi <kupourevans99@gmail.com>
2024-04-26 16:02:15 +0000

    Add Studdent enrollment definitions

--- Schools_Admission/classes/views.py
+++ Schools_Admission/classes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render #type: ignore
 
-from rest_framework import viewsets #type: ignore
+from rest_framework import viewsets, status #type: ignore
 from .models import Class
 from .serializers import ClassSerializer
 from rest_framework.response import Response  #type:ignore
@@ -10,6 +10,16 @@ from rest_framework.response import Response  #type:ignore
 class ClassViewSet(viewsets.ModelViewSet):
     queryset = Class.objects.all()
     serializer_class = ClassSerializer
+    
+    def enroll_student(self, request, pk=None):
+        try:
+            class_instance = self.get_object()
+            if class_instance.enroll_student():
+                return Response({'message': 'Student enrolled successfully'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'message': 'Class is already full'}, status=status.HTTP_400_BAD_REQUEST)
+        except Class.DoesNotExist:
+            return Response({'message': 'Class not found'}, status=status.HTTP_404_NOT_FOUND)
 
 
 class SchoolClassesViewSet(viewsets.ViewSet):
